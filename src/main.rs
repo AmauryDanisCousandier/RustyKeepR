d0a4571626e79d896d15c5efdfec23d1da6f377f
danis <amaury.danis-cousandier@epitech.eu>
2023-10-03 23:24:46 +0200

    Item struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+struct Item {
+    name: String,
+    price: f64,
+    quantity: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _item = Item {
+        name: String::from("parsnip"),
+        price: 5.99,
+        quantity: 14,
+    };
+    println!("I have {} {} at {}$ each.",_item.quantity , _item.name, _item.price);
 }
