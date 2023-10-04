365f3b2b822e1b989b295606d05de2d7d8e524cd
danis <amaury.danis-cousandier@epitech.eu>
2023-10-04 14:14:07 +0200

    vector yay

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod inventory;
 
 fn main() {
+    let mut _vec: Vec<inventory::Item> = Vec::new();
     inventory::inventory::add_new_item();
     inventory::inventory::remove_item();
     inventory::inventory::display_inventory();
@@ -11,5 +12,6 @@ fn main() {
         price: 5.99,
         quantity: 14,
     };
+
     println!("I have {} {} at {}$ each.",_item.quantity , _item.name, _item.price);
 }
